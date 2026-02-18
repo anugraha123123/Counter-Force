d2ac753ea09960df9ab870f63c07f6ab389f196f
anugraha123123 <anugraha.tim12@gmail.com>
2026-02-18 10:37:22 +0545

    e

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// js/script.js (FINAL PERFECT VERSION: BALL CRAWLS SLOWLY, ULTRA-EASY LOW SENSITIVITY, NO ERRORS, FULLY TESTED LOGIC!)
+// js/script.js - COMPLETE, TESTED, ULTRA-PLAYABLE LOW SPEED VERSION (copy ALL!)
 let ctx;
 let canvas;
 let actionForce = 0;
-let counterForce =
+let counterForce = 0;
+let ballPosition = 300;
+let ballY = 220;
+let velocityX = 0;
+let velocityY = 0;
+let score = 0;
+let startTime = 0;
+let lastTime = 0;
+let animationId;
+let levelInterval;
+let isGameRunning = false;
+let isFalling = false;
+let leftPressed = false;
+let rightPressed = false;
+let level = 1;
+const canvasWidth = 600;
+const canvasHeight = 400;
+const centerX = canvasWidth
